123c3d2534e5fbb56c4de139e11b794ec59d2b68
Jude Aakjaer <santouras@gmail.com>
2012-05-29 15:46:30 +1000

    changes for zf2 sm

--- module/Foosball/config/module.config.php
+++ module/Foosball/config/module.config.php
@@ -1,14 +1,29 @@
 <?php
 return array(
-    'di' => array(
-        'instance' => array(
-            'Zend\View\Resolver\TemplatePathStack' => array(
-                'parameters' => array(
-                    'paths'  => array(
-                        'album' => __DIR__ . '/../view',
+    // Routes for this module
+    'router' => array(
+        'routes' => array(
+            'foosball' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/foosball[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'foosball/foosball',
+                        'action'     => 'index',
                     ),
                 ),
             ),
+        ),
+    ),    
+
+    // View setup for this module
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'foosball' => __DIR__ . '/../view',
         ),
     ),
 );
