b872df9e23cbf4355dc2dfc30cc177c9c2bea64a
Jude Aakjaer <santouras@gmail.com>
2012-06-04 22:00:42 +1000

    add in Player model, update PlayerTable to new abstract reference

---
+++ module/Foosball/src/Foosball/Model/Player.php
@@ -0,0 +1,9 @@
+<?php
+namespace Foosball\Model;
+
+use Zend\Db\ResultSet\Row;
+
+class Album extends Row
+{
+    
+}
--- module/Foosball/src/Foosball/Model/PlayerTable.php
+++ module/Foosball/src/Foosball/Model/PlayerTable.php
@@ -7,8 +7,19 @@ use Zend\Db\ResultSet\ResultSet;
 
 class PlayerTable extends TableGateway
 {
-    public function __construct(Adapter $adapter = null, $databaseSchema = null, ResultSet $selectResultPrototype = null)
+    public function __construct(Adapter $adapter = null)
     {
-        parent::__construct('player', $adapter, $databaseSchema, $selectResultPrototype);
+        $this->adapter = $adapter;
+        $this->resultSetPrototype = new ResultSet(new Player);
+
+        $this->initialize();
+    }
+
+    public function load()
+    {
+    }
+
+    public function loaded()
+    {
     }
 }
