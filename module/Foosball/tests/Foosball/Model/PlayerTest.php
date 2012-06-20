6deb6740061810ec1af6f0211d23c69223820c51
Jude Aakjaer <santouras@gmail.com>
2012-06-21 09:32:03 +1000

    adding player test case

---
+++ module/Foosball/tests/Foosball/Model/PlayerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FoosballTest\Model;
+
+use Foosball\Model\Player;
+
+class PlayerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Our Player
+     * @var Foosball\Model\Player
+     */
+    protected $p;
+
+    public function setup ()
+    {
+        $this->p = new Player();
+    }
+
+    public function testNotLoaded() 
+    {
+        $this->assertFalse($this->p->loaded());
+    }
+}
