02811be0f41e86688012ec49f9082cd147e7f10d
Jude Aakjaer <jude@itomic.com.au>
2012-07-10 19:51:03 +1000

    fixes to bootstrap for phpunit

--- module/Foosball/tests/Bootstrap.php
+++ module/Foosball/tests/Bootstrap.php
@@ -1,56 +1,8 @@
 <?php
-use Zend\ServiceManager\ServiceManager;
-use Zend\Mvc\Service\ServiceManagerConfiguration;
-
-/*
- * Set error reporting to the level to which Zend Framework code must comply.
- */
-error_reporting( E_ALL | E_STRICT );
-
-$phpUnitVersion = PHPUnit_Runner_Version::id();
-if ('@package_version@' !== $phpUnitVersion && version_compare($phpUnitVersion, '3.5.0', '<')) {
-    echo 'This version of PHPUnit (' . PHPUnit_Runner_Version::id() . ') is not supported in Zend Framework 2.x unit tests.' . PHP_EOL;
-    exit(1);
-}
-unset($phpUnitVersion);
-
-/*
- * Determine the root, library, and tests directories of the framework
- * distribution.
- */
-$docRoot        = realpath(dirname(__DIR__) . '/../../');
-$zfCore = "$docRoot/vendor/ZendFramework";
-$foosballTests   = "$docRoot/module/Foosball/tests";
-
-/*
- * Prepend the Zend Framework library/ and tests/ directories to the
- * include_path. This allows the tests to run out of the box and helps prevent
- * loading other copies of the framework code and tests that would supersede
- * this copy.
- */
-$path = array(
-    $zfCore . '/library',
-    $foosballTests,
-    get_include_path(),
-);
-set_include_path(implode(PATH_SEPARATOR, $path));
-
-/**
- * Setup autoloading
- */
-include __DIR__ . '/_autoload.php';
-include $zfCore . '/tests/_autoload.php';
-
-/*$configuration = include $docRoot . '/config/application.config.php';
-
-// setup service manager
-$serviceManager = new ServiceManager(new ServiceManagerConfiguration($configuration['service_manager']));
-$serviceManager->setService('ApplicationConfiguration', $configuration);
-$serviceManager->get('ModuleManager')->loadModules();*/
+chdir(dirname(__DIR__) . '/../../');
 
 
-/*
- * Unset global variables that are no longer needed.
- */
-unset($zfRoot, $zfCore, $foosballTests, $path, $serviceManager);
+include __DIR__ . '/../../../init_autoloader.php';
 
+Zend\Mvc\Application::init(include 'config/application.config.php');
+chdir(__DIR__);
