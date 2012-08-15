8a14106105c422b49fc1b5513ee2c2e570e24b65
Jude Aakjaer <jude@itomic.com.au>
2012-08-15 15:57:25 +1000

    add firePHP logger

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -4,6 +4,7 @@ namespace Application;
 
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Db\Adapter\Adapter as DbAdapter;
+use Zend\Log\Logger;
 
 class Module
 {
@@ -27,6 +28,20 @@ class Module
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
                 ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Zend\Log\Logger' =>  function($sm) {
+                    $logger = new Logger;
+                    $writer = $sm->get('Zend\Log\Writer\FirePHP');
+                    $logger->addWriter($writer);
+                    return $logger;
+                },
             ),
         );
     }
